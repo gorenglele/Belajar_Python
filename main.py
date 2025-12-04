642f81a683066d308ba6a558315daea47cb34055
LENDRA SYAPUTRA <l3noneone@gmail.com>
2025-12-04 17:05:17 +0700

    Day 1 Belajar Piton

---
+++ main.py
@@ -0,0 +1,29 @@
+print("Hallo Dunia - Hello World")
+
+string = str
+nama = "FAza"
+
+integer = int
+angka = 24
+
+boolean = bool
+benar = True
+salah = False
+
+floating = float
+nilai = 98.2
+
+lists = list
+buah = ["apel", "jeruk", "mangga"]
+
+tuples = tuple
+namaIT = ("Eka", "Budi", "Faza")
+
+
+print(nama)
+print(angka)
+print(benar)
+print(salah)
+print(nilai)
+print(buah)
+print(namaIT)
