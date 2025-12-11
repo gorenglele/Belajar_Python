7b85b52342684f8f4e2730813f3c561b99d1dc41
LENDRA SYAPUTRA <l3noneone@gmail.com>
2025-12-11 16:25:18 +0700

    Day 2 belajar pyton

---
+++ variable.py
@@ -0,0 +1,16 @@
+nama_variabel = "FAza"
+angka_eka = 20
+angka_faza = 10
+
+angka_legor = angka_eka
+angkafaza = angka_eka
+
+print(angka_legor)
+print(angka_eka)
+print(angka_faza)
+print(angka_faza)
+
+print(nama_variabel)
+
+# None = nama_variabel
+
