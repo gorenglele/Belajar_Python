734f9598b5f1b7a51e242581e43c2f8e78143b8b
LENDRA SYAPUTRA <l3noneone@gmail.com>
2025-12-18 16:59:36 +0700

    Day 3 belajar python

---
+++ operator.py
@@ -0,0 +1,19 @@
+angka = 5
+
+angka_faza = 5
+
+print(angka + angka_faza) #penjumlahan
+print(angka - angka_faza) #pengurangan
+print(angka * angka_faza) #perkalian
+print(angka / angka_faza) #pembagian
+print(angka % angka_faza) #modulus
+print(angka ** angka_faza) #pangkat
+print(angka // angka_faza) #floor
+
+
+print(angka > angka_faza)
+print(angka < angka_faza)
+print(angka != angka_faza)
+print(angka == angka_faza)
+print(angka >= angka_faza)
+print(angka <= angka_faza)
