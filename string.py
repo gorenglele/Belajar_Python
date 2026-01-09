6707ce67d0e4d5db1cc7c7cb2d4d89ebb9325ebe
LENDRA SYAPUTRA <l3noneone@gmail.com>
2026-01-09 14:53:18 +0700

    Day 4 belajar python

---
+++ string.py
@@ -0,0 +1,16 @@
+kata_pertama = "legor"
+
+kata_eka = 'ALOk'
+
+kata_faza = """eka adalah seorang pro-gamer"""
+
+print(kata_pertama)
+print(kata_eka)
+print(kata_faza)
+
+print(kata_eka.upper())
+print(kata_eka.islower())
+
+print(kata_pertama[4])
+
+print(len(kata_eka))
