40e429e598d2b2472060f20c8511540d3a2af0f6
gorenglele <lendra@bignet.id>
2026-01-29 16:54:48 +0700

    Belajar python day 6

---
+++ conditional.py
@@ -0,0 +1,19 @@
+cuaca = "Hujan"
+
+if cuaca == "Hujan":
+    print("Bawa payung")
+else:
+    print("Tidak perlu bawa payung")
+
+nilai = 81
+
+if nilai >= 100:
+    print("Nilai Anda A")
+elif nilai >= 80:
+    print("Nilai Anda B")
+elif nilai >= 60:
+    print("Nilai Anda C")
+elif nilai >= 40:
+    print("Nilai Anda D")
+else:
+    print("Nilai Anda E")
